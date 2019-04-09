89963fa241247aedd7fd72fd28daecbd6b241891
“David <dakaspar@gmail.com>
2019-04-09 14:20:17 -0700

    create classes in .py file

---
+++ driver.py
@@ -0,0 +1,11 @@
+class Ride():
+    def __init__(self):
+        self.start_time = 'start unknown'
+        self.stop_time = 'stop unknown'
+        
+class Driver():
+    name = 'Jon Though'
+    
+class Passenger():
+    name = 'Janonymous'
+    
